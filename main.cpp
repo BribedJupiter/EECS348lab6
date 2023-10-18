0d25f2cb6cf410b7497e3b2ec13ce60cb3fdd595
jacktop-ubuntu <jgb1017@gmail.com>
2023-10-18 14:27:14 -0500

    Prep work progress

--- main.cpp
+++ main.cpp
@@ -1,8 +1,74 @@
 #include <stdio.h>
-#define MAX_SIZE = 100
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void readFile(int* refArray)
+{
+
+}
+
+void printMatrix(int* refArray)
+{
+
+}
+
+void addMatrix(int* refArrayOne, int* refArrayTwo)
+{
+
+}
+
+void multMatrix(int* refArrayOne, int* refArrayTwo)
+{
+
+}
+
+void subtractMatrix(int* refArrayOne, int* refArrayTwo)
+{
+
+}
 
 int main ()
-{
-    printf("Hello world\n");
+{   
+    int matrix[100][100]; // The max size of our matrix will be 100x100
+    int size;
+    char filename[20]; // Create a buffer for our filename
+    
+     // Get a file name from the user to read in the matrix data
+    cout << "Enter an input filename: ";
+    cin >> filename;
+
+    // Open the file and get a pointer to it
+    FILE* inputFilePtr = fopen(filename, "r");
+
+    // Make sure it was a valid file
+    if (inputFilePtr == NULL)
+    {
+        cout << "Invalid filename" << endl;
+        return 0;
+    }
+
+    // I need to be able to take the file and turn it into a 2D array format 
+    /*
+        Open file
+        Read first char --> determine loop amt
+        Loop through lines
+            Create row from line
+            Add row to matrix
+    */
+
+    /* Note: This assumes the file is 12 lines long with a number on each line and only that */
+    /* Read in the file and store the data in the sales array */
+    /*for (int i = 0; i < 12; i++)
+    {
+        fscanf(inputFilePtr, "%f", &[i]);
+    }*/
+
+    
+    /* Close the file as keeping it open is no longer needed */
+    fclose(inputFilePtr);
+     
     return 0;
 }
